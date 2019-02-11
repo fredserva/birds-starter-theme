ab79f469034dfe8e2b09435319e40984b0d316b6
fredserva <fred.serva@gmail.com>
2019-02-11 11:00:34 +0100

    Skip Link Focus

--- js/skip-link-focus-fix.js
+++ js/skip-link-focus-fix.js
@@ -1,36 +1,78 @@
 /**
- * Makes "skip to content" link work correctly in IE9, Chrome, and Opera
- * for better accessibility.
+ * Skip Link Focus v1.0.0
+ * https://github.com/cedaro/skip-link-focus
+ *
+ * @copyright Modifications Copyright (c) 2015 Cedaro, LLC
+ * @license BSD-3-Clause
+ */
+
+/**
+ * Make "skip to content" links work correctly in IE9, Chrome, and Opera to
+ * improve accessibility.
  *
  * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
  */
+(function( root, factory ) {
+	'use strict';
 
- ( function() {
-	var isWebkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-		isOpera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-		isIE     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
-
-	if ( ( isWebkit || isOpera || isIE ) && document.getElementById && window.addEventListener ) {
-		window.addEventListener( 'hashchange', function() {
-			var id = location.hash.substring( 1 ),
-				element;
-
-			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
-				return;
-			}
-
-			element = document.getElementById( id );
-
-			if ( element ) {
-				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
-					element.tabIndex = -1;
-				}
-
-				element.focus();
-
-				// Repositions the window on jump-to-anchor to account for admin bar and border height.
-				window.scrollBy( 0, -53 );
-			}
-		}, false );
+	if ( 'function' === typeof define && define.amd ) {
+		define( [], factory );
+	} else if ( 'object' === typeof exports ) {
+		module.exports = factory();
+	} else {
+		root.skipLinkFocus = factory();
 	}
-} )();
+}( this, function() {
+	'use strict';
+
+	function init( options ) {
+		options = options || {};
+		options.selector = options.selector || '.skip-link';
+
+		if ( window && /webkit|opera|msie|trident/i.test( navigator.userAgent ) && window.addEventListener ) {
+			var i,
+				skipLinks = window.document.querySelectorAll( options.selector );
+
+			window.addEventListener( 'hashchange', function() {
+				skipToElement( location.hash.substring( 1 ) );
+			}, false );
+
+			// Fix for when the address bar already contains a hash.
+			for ( i = 0; i < skipLinks.length; ++i ) {
+				skipLinks[ i ].addEventListener( 'click', skipLinkClickHandler );
+			}
+
+			// Handle initial hash.
+			if ( location.hash && location.hash.substring( 1 ) ) {
+				skipToElement( location.hash.substring( 1 ) );
+			}
+		}
+	}
+
+	function skipLinkClickHandler( e ) {
+		skipToElement( e.target.hash.substring( 1 ) );
+	}
+
+	function skipToElement( id ) {
+		var element;
+
+		if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+			return;
+		}
+
+		element = window.document.getElementById( id );
+
+		if ( element ) {
+			if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+				element.tabIndex = -1;
+			}
+
+			element.focus();
+		}
+	}
+
+	return {
+		init: init,
+		skipToElement: skipToElement
+	};
+}));
